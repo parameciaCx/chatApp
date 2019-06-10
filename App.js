c08e4eb5d8c44c5c478f9f322adb0a4221fc9594
Zizhou Zhang <strawhatj@gmail.com>
2019-06-10 16:00:43 +0800

    important parts of chat functional

--- App.js
+++ App.js
@@ -1,1 +1,30 @@
 const socket = io('http://localhost:3231');
+
+const display = document.getElementById('root');
+const cont = document.getElementById('msgContainer');
+const inp = document.getElementById('msgInput');
+
+const userName = prompt('Enter display name: ');
+appendMsg('Connected to chatroom.');
+socket.emit('new-user', userName);
+
+socket.on('append-message', (data) => {
+	appendMsg(`${data.id}: ${data.msg}`);
+});
+
+socket.on('user-joined', (user) => {
+	appendMsg(`User ${user} connected.`);
+});
+
+cont.addEventListener('submit', (e) => {
+	e.preventDefault();
+	appendMsg(`${userName}: ${inp.value}`);
+	socket.emit('send-message', inp.value);
+	inp.value = '';
+});
+
+function appendMsg(message) {
+	const msg = document.createElement('div');
+	msg.innerText = message;
+	display.append(msg);
+}
--- SocketManager.js
+++ SocketManager.js
@@ -1,4 +1,15 @@
 const io = require('./server.js').io;
+
+const users = {};
 module.exports = function(socket) {
 	console.log(socket.id);
+
+	socket.on('new-user', (userName) => {
+		users[socket.id] = userName;
+		socket.broadcast.emit('user-joined', userName);
+	});
+
+	socket.on('send-message', (message) => {
+		socket.broadcast.emit('append-message', { msg: message, id: users[socket.id] });
+	});
 };
